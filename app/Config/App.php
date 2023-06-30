c639efb4548f352075e040ad1603c7e8c6b7e703
Litbang Bungamayang <litbang.bungamayang@gmail.com>
2023-06-30 23:39:08 +0700

    [+] Repair commit

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://localhost/kebun-2/';
+	public $baseURL = 'http://localhost/kantor/';
 
 	/**
 	 * --------------------------------------------------------------------------
